aa90fd14ea910263800d2e9a035e73e3866fb4b2
Jonathan Di Rico <yonasuriv@localhost.localdomain>
2021-11-18 01:29:43 +0100

    Updated installer.sh

--- system/apps-scripts/installer.sh
+++ system/apps-scripts/installer.sh
@@ -1,3 +1,5 @@
+sudo apt-get install npm
+sudo apt install snapd
 sh discover.sh
 sh brave.sh
 sh guaketerminal.sh
@@ -5,3 +7,7 @@ sh spotify.sh
 sh virtualbox.sh
 sh p10k.sh
 echo Visual Studio Code automatic installer was disabled, it needs to be manually installed due to conflicts with VSCodium.
+echo If you care about security and privacy, please use VSCodium and migrate your extensions.
+echo Stremio can be downloaded here: https://www.stremio.com/downloads
+echo P3X One Note is not linked!
+echo To do so, run: sudo snap install p3x-onenote
